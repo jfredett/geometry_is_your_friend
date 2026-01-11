403ed7854f9a0cee34efbaec8f5b99698ad427d2
Joe Fredette <jfredett@gmail.com>
2026-01-10 21:09:29 -0500

    prove 2.4

--- Geometry/Ch2/Prop/P4.lean
+++ Geometry/Ch2/Prop/P4.lean
@@ -17,8 +17,15 @@ open Geometry.Ch2.Defs
 variable (G : IncidenceGeometry)
 
 -- p71. "For every point, there is at least one line not passing through it."
-theorem P4 {G : IncidenceGeometry} :
-    ∀ P : G.Point, ∃ L : G.Line, ¬(G.Incident P L) := by
-    sorry
+theorem P4 (P : G.Point) : ∃ L : G.Line, (P off L) := by
+    -- Similar to 2.3, but using 2.2's configuration.
+    obtain ⟨L, M, N, hDistinct, hNC⟩ := Geometry.Ch2.Prop.P2
+    unfold Concurrent at hNC
+    by_contra! hNeg
+    push_neg at *
+    specialize hNC P
+    have PonN := hNeg N
+    have PoffN := hNC (hNeg L) (hNeg M)
+    contradiction
 
 end Geometry.Ch2.Prop
