a39e54ccf05bfe1ef4db9d5581ada98e8adae56c
Joe Fredette <jfredett@gmail.com>
2026-01-10 20:49:37 -0500

    work on custom prop/lemma/corollary/exercise syntax

--- Geometry/Syntax.lean
+++ Geometry/Syntax.lean
@@ -2,28 +2,55 @@ module
 
 import Lean
 
+open Lean
+
 /-
   =====================================================
   Textbook-style declarations with informal statements
   =====================================================
 -/
 
-/-- proposition ch.p "informal statement" name := proof -/
-syntax "proposition" num "." num str term : command
+/-- proposition ch.p statement := proof -/
+syntax (name := propositionDecl)
+  "proposition" num "." num term ":=" term : command
+/-- exercise ch.e name := proof -/
+syntax (name := exerciseDecl)
+  "exercise" num "." num term ":=" term : command
 
-/-- exercise ch.e "informal statement" name := proof -/
-syntax "exercise" num "." num str term : command
+syntax (name := corrolaryDecl)
+  "corollary" num "." num "." num term ":=" term : command
+
+syntax (name := lemmaDecl)
+  "lemma" num "." num "." num term ":=" term : command
+
+-- FIXME: Should probably parse directly, also this screws up the implicit def,
+-- I think probably it's `terms : command`, as in, multiple terms, then the
+-- command?
+-- FIXME: Technically lemma is already taken, but this should parse it correctly anyway
+-- TODO: I dropped the 'informal statement as part of the proposition command itself', leaving
+-- the documentation as just a normal doc comment; I don't know that I care enough to implement
+-- it for real.
+macro_rules
+  | `(proposition $chapter . $prop $stmt := $proof) =>
+      let rawStr := s!"Geometry.Ch{chapter}.Prop.P{prop}"
+      let name := Lean.mkIdent rawStr.toName
+      `(theorem $name : $stmt := $proof)
+  | `(exercise $chapter . $ex $stmt := $proof) =>
+      let rawStr := s!"Geometry.Ch{chapter}.Prop.Ex{ex}"
+      let name := Lean.mkIdent rawStr.toName
+      `(theorem $name : $stmt := $proof)
+  | `(corollary $chapter . $prop . $co $stmt := $proof) =>
+      let rawStr := s!"Geometry.Ch{chapter}.Prop.P{prop}.C{co}"
+      let name := Lean.mkIdent rawStr.toName
+      `(theorem $name : $stmt := $proof)
+  | `(lemma $chapter . $prop . $lem $stmt := $proof) =>
+      let rawStr := s!"Geometry.Ch{chapter}.Prop.P{prop}.L{lem}"
+      let name := Lean.mkIdent rawStr.toName
+      `(theorem $name : $stmt := $proof)
+
+
 
 /-
-macro_rules
-  | `(proposition $ch:num . $p:num $doc:str $val:term) =>
-      `(
-        namespace Geometry.Chapter.C$ch.Prop
-        theorem P$p : _ := $val
-        @[doc $doc]
-        end Geometry.Chapter.C$ch.Prop
-      )
-
 macro_rules
   | `(exercise $ch:num . $e:num $doc:str $val:term) =>
       `(
