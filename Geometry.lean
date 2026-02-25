62a313aa220dd90d02f4f2922e63fe3e6c5a07fa
Joe Fredette <jfredett@gmail.com>
2026-02-25 16:07:24 -0500

    cleanup

--- Geometry.lean
+++ Geometry.lean
@@ -1,17 +1,15 @@
-
 -- Chapter 2
-
--- -- Propositions
+-- Propositions
 import Geometry.Ch2.Prop.P1
 import Geometry.Ch2.Prop.P2
 import Geometry.Ch2.Prop.P3
 import Geometry.Ch2.Prop.P4
 import Geometry.Ch2.Prop.P5
-/- -- -- Exercises -/
-/- -- public import Geometry.Ch2.Ex.E1 -/
+-- Exercises -/
+-- import Geometry.Ch2.Ex.E1
 
-/- -- Chapter 3 -/
-/- -- -- Propositions -/
+-- Chapter 3
+-- Propositions
 import Geometry.Ch3.Prop.P1
 import Geometry.Ch3.Prop.B4iii -- A corrolary of B4, nothing without proof.
 import Geometry.Ch3.Prop.P2
