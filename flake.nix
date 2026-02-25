0357ca87344d0baa6630189d0cae55cbbe6e2046
Joe Fredette <jfredett@gmail.com>
2026-02-24 22:15:34 -0500

    add flake and requirements.txt

---
+++ flake.nix
@@ -0,0 +1,105 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #    devshell.url = "github:numtide/devshell";
+    # process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    nixpkgs-python = {
+      url = "github:cachix/nixpkgs-python";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-python, flake-parts, ... } @ inputs: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+
+      perSystem = { config, pkgs, system, ... }: with builtins; let
+        collect = f: deps: concatMap f (attrValues deps);
+        pythonFlake = nixpkgs-python.packages.${system};
+        pythonInterp = pythonFlake."3.13.1";
+        pip = pkgs.python3Packages;
+        ld_deps = [
+          pkgs.stdenv.cc.cc
+        ];
+        deps = with pkgs; {
+          python = {
+            dev = [];
+            ci = [
+              pythonInterp
+              pip.pip
+              pip.venvShellHook
+            ];
+          };
+
+          # General tooling
+          tools = {
+            dev = [
+              bc
+              cloc
+              curl
+              eplot
+              gnuplot
+              jq
+              just
+              pandoc
+              ripgrep
+              timg
+              watch
+              yq-go
+            ];
+            ci = [];
+          };
+        };
+
+        ci_deps = collect (v: v.ci) deps;
+        dev_deps = (collect (v: v.dev) deps) ++ ci_deps;
+
+
+      in {
+        packages = {
+          ci = pkgs.writeShellApplication {
+            name = "ci";
+            runtimeInputs = deps.ci;
+            text = /* bash */ ''
+              just ci
+            '';
+          };
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            name = "dev shell";
+            venvDir = "./.venv";
+            nativeBuildInputs = [ ];
+            buildInputs = ci_deps;
+            packages = dev_deps;
+
+            shellHook = /* bash */ ''
+              SOURCE_DATE_EPOCH=$(date +%s)
+              VENV=.venv
+
+              if test ! -d $VENV; then
+                python -m venv $VENV
+              fi
+              source ./$VENV/bin/activate
+              export PYTHONPATH=`pwd`/$VENV/${pkgs.python3.sitePackages}/:$PYTHONPATH
+              pip install -r requirements.txt
+            '';
+
+            postShellHook = /* bash */ ''
+              export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath ld_deps}"
+              # NOTE: Suppresses warnings about future deprecations in az-cli, these are outside our control generally.
+              # not an ideal solution to this.
+              export PYTHONWARNINGS="ignore::FutureWarning"
+            '';
+          };
+        };
+      };
+    };
+}
+
