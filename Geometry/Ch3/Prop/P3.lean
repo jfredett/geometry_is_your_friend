747de3aae1c7628c357c7848569c9ea186400dec
Joe Fredette <jfredett@gmail.com>
2026-03-03 14:46:48 -0500

    use distinguish/separate on 3.3

--- Geometry/Ch3/Prop/P3.lean
+++ Geometry/Ch3/Prop/P3.lean
@@ -23,29 +23,26 @@ open Geometry.Ch3.Prop
 open Geometry.Ch3.Ex
 
 /-- p112. Given A - B - C and A - C - D, then B - C - D and A - B - D (see Figure 3.9) -/
-theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D := by 
+theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D := by
   /- (1) A, B, C, and D are distinct, collinear points (see Exercise 1). -/
   intro A B C D ⟨ABC, ACD⟩
   have distinctABCD := Ex1.a ⟨ABC, ACD⟩
-  have ⟨colABC, colBCD⟩ := Ex1.b ⟨ABC, ACD⟩
+  separate at distinctABCD
+  have colABCD := Ex1.b ⟨ABC, ACD⟩
+  let L := colABCD.line
   /- (2) There exists a point E not on the line through A,B,C,D (Proposition 2.3) -/
   -- NOTE: WLOG, we can pick either of colABC or colBCD because all these points are collinear
-  have ⟨L, AonL, BonL, ConL⟩ := colABC
-  have AneB : A ≠ B := by distinguish distinctABCD A B
-  have AneC : A ≠ C := by distinguish distinctABCD A C
-  have BneC : B ≠ C := by distinguish distinctABCD B C
-  have CneD : C ≠ D := by distinguish distinctABCD C D
   have LeqAB : L = line A B := Line.equiv AneB
-    ⟨AonL, Line.line_has_definition_points.left, BonL, Line.line_has_definition_points.right⟩
+    ⟨colABCD.mem A, Line.line_has_definition_points.left, colABCD.mem B, Line.line_has_definition_points.right⟩
   have ⟨E, EoffL⟩ := Ch2.Prop.P3 L
   /- (3) Consider line EC. Since (by hypothesis) AD meets this line in point C,... -/
   let EC := line E C
-  -- <Ed> Missing these simple conditions
+  -- {Ed} Missing these simple conditions
   have ConEC : C on EC := Line.line_has_definition_points.right
   have LneEC : L ≠ EC := by
     have EonEC : E on EC := Line.line_has_definition_points.left
     by_contra! hNeg; rw [hNeg] at EoffL; contradiction
-  have ConLintEC : C on L ∩ EC := ⟨ConL, ConEC⟩
+  have ConLintEC : C on L ∩ EC := ⟨colABCD.mem C, ConEC⟩
   have LnparEC : L ∦ EC := by
     by_contra! hNeg
     have emptyInter := Intersection.parallel_intersection_is_empty L EC LneEC hNeg
@@ -54,29 +51,26 @@ theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D 
   have LintECatC : L intersects EC at C := (Intersection.single_point_of_intersection C L EC ⟨LneEC, LnparEC⟩).mp ConLintEC
   have AoffEC : A off EC := by
     by_contra! hNeg
-    have AonInt : A ∈ L ∩ EC := ⟨AonL, hNeg⟩
-    exfalso
+    have AonInt : A ∈ L ∩ EC := ⟨colABCD.mem A, hNeg⟩
     have AeqC := Intersection.intersection_is_unique L EC LneEC LnparEC ⟨AonInt, ConLintEC⟩
     contradiction
   have BoffEC : B off EC := by
     by_contra! hNeg
-    have BonInt : B ∈ L ∩ EC := ⟨BonL, hNeg⟩
-    have BneC : B ≠ C := by simp_all only [B1a, ne_eq, not_false_eq_true]
+    have BonInt : B ∈ L ∩ EC := ⟨colABCD.mem B, hNeg⟩
     have BeqC := Intersection.intersection_is_unique L EC LneEC LnparEC ⟨BonInt, ConLintEC⟩
     contradiction
   have DonL : D on L := by rw [LeqAB]; unfold LineThrough; simp only [mem_setOf_eq]; sorry
   have DoffEC : D off EC := by
     by_contra! hNeg
     have DonInt : D ∈ L ∩ EC := ⟨DonL, hNeg⟩
-    have DneC : D ≠ C := Ne.symm (Betweenness.abc_imp_distinct.bnec ACD)
+    have DneC : D ≠ C := CneD.symm
     have DeqC := Intersection.intersection_is_unique L EC LneEC LnparEC ⟨DonInt, ConLintEC⟩
     contradiction
-  -- </Ed>
+  -- {/Ed}
   /- ... points A and D are on opposite sides of EC -/
   have ECsplitsAandD : EC splits A and D := by
     unfold SameSide; push_neg
     intro AoffEC DoffEC
-    have AneD : A ≠ D := by distinguish distinctABCD A D
     refine ⟨AneD, ?_⟩
     use C
     -- TODO: This gets cleaner with better collinearity
@@ -90,17 +84,18 @@ theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D 
       · exfalso; rwa [LparEC] at ConLintEC
       · exact LintECatX
       · exfalso; rw [<- LeqEC] at AoffEC; contradiction
-    -- <Ed> need this for a step below
+    -- {Ed} need this for a step below
     have XinIntLine : X ∈ L ∩ EC := by
       simp only [mem_inter_iff]; simp only at LintECatX;
       refine (mem_inter_iff X L EC).mp ?_
       rw [LintECatX]
       exact mem_singleton X
     have ⟨XonL, XonEC⟩ := XinIntLine
-    -- </Ed>
+    -- {/Ed}
     have AXB : A - X - B := by
-      have colAXB : Collinear A X B := by
+      have colAXB : collinear A X B := by
         use L
+        sorry
       have AneX : A ≠ X := by
         by_contra! hNeg
         rw [hNeg] at AoffEC
@@ -109,8 +104,9 @@ theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D 
         by_contra! hNeg
         rw [hNeg] at BoffEC
         contradiction
+      have distinctAXB : distinct A X B := by separate; tauto
       -- TODO: I think a better argument exists here to reject the two alternative cases.
-      rcases B3 A X B ⟨AneX, BneX.symm, AneB, colAXB⟩ with ⟨AXB, _⟩ | ⟨_, XAB, _⟩ | ⟨_, _, ABX⟩
+      rcases B3 A X B ⟨distinctAXB, colAXB⟩ with ⟨AXB, _⟩ | ⟨_, XAB, _⟩ | ⟨_, _, ABX⟩
       · exact AXB
       · exfalso
         have ECguardsAB : EC guards A and B := by
@@ -163,7 +159,7 @@ theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D 
       contradiction
     /- (10) Hence, the point C of intersection of lines EC and BD lies between B and D (definition of "opposite sides";
        Proposition 2.1, i.e., that the point of intersection is unique). -/
-    have BCD : B - C - D := by 
+    have BCD : B - C - D := by
       unfold SameSide at ECsplitsBandD
       push_neg at ECsplitsBandD
       specialize ECsplitsBandD BoffEC DoffEC
@@ -182,7 +178,7 @@ theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D 
   /- A similar argument involving EB proves that A - B - D -/
 
   /- TODO: Ed: time to break out `suffices` or some other clever thing... -/
-  
+
 
 -- theorem P3.right : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> B - C - D := by sorry
 -- theorem P3.left : ∀ A B C D : Point, (A - B - C) ∧ (A - C - D) -> A - B - D := by sorry
